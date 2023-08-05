fe3e6edcbd1724052e359c13335982f9316ac1a6
Andrew Cardin <cardin.andrew@gmail.com>
2023-08-05 04:12:42 -0500

    Creates basic TCP/IP Echo Server

---
+++ AvendyrServer/AvendyrServer/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        const int port = 13000;
+
+        var localAddr = IPAddress.Parse("127.0.0.1");
+
+        var server = new TcpListener(localAddr, port);
+
+        server.Start();
+
+        Console.WriteLine("Waiting for a connection...");
+
+        while (true)
+        {
+            var client = await server.AcceptTcpClientAsync();
+
+            // Pass off to a separate method so we can await a new client immediately.
+            HandleClientAsync(client);
+        }
+    }
+
+    static async void HandleClientAsync(TcpClient client)
+    {
+        var bytes = new byte[256];
+        Console.WriteLine("Connected!");
+
+        var stream = client.GetStream();
+
+        int i;
+
+        while ((i = await stream.ReadAsync(bytes, 0, bytes.Length)) != 0)
+        {
+            // Translate data bytes to an ASCII string and print it
+            var data = Encoding.ASCII.GetString(bytes, 0, i);
+            Console.WriteLine($"Received: {data}");
+
+            // Process the data sent by the client and send back a response
+            data = data.ToUpper();
+
+            var msg = Encoding.ASCII.GetBytes(data);
+
+            // Send back a response.
+            await stream.WriteAsync(msg, 0, msg.Length);
+            Console.WriteLine($"Sent: {data}");
+        }
+
+        client.Close();
+    }
+}
